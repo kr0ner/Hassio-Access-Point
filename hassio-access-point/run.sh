30a92a48cc966d778c211736e77e6b63a9f193b5
Daniel <dkroner87@gmail.com>
2025-02-13 22:56:01 +0100

    Update run.sh

--- hassio-access-point/run.sh
+++ hassio-access-point/run.sh
@@ -204,6 +204,8 @@ fi
 echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
 iptables -A FORWARD -i wlan0 -o lo -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
+ip route add default dev wlan0
+sysctl -w net.ipv4.ip_forward=1
 
 # Start dnsmasq if DHCP is enabled in config
 if $(bashio::config.true "dhcp"); then
